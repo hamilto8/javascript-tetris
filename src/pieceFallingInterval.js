0c1af22be19799dc2130179e5e954ff3e3b77a89
Michael <michael.ts.hamilton@gmail.com>
2021-03-29 21:45:06 -0500

    Continued work on adding rotation ability to tetrominos.

--- src/pieceFallingInterval.js
+++ src/pieceFallingInterval.js
@@ -9,6 +9,9 @@ let pieceFallingInterval;
 let currentPiece = currentPieceIdx["I"]["POS1_IDX"];
 
 const pieceFalling = () => {
+    // for(let i = 0; i < currentPieceIdx["I"]["POS2_IDX"].length; i++){
+    //     currentPieceIdx["I"]["POS2_IDX"][i] += 10;
+    // }
     if(currentPieceIdx["I"]["POS"] === 1){
         if(currentPiece[3]+10 < boardArr.length){
             makeBlank(currentPiece[0]);
@@ -24,19 +27,34 @@ const pieceFalling = () => {
             clearInterval(pieceFallingInterval);
         } 
     } else if (currentPieceIdx["I"]["POS"] === 2){
-        if(currentPiece[3]+10 < boardArr.length){
-            makeBlank(currentPiece[0]);
-            for(let i = 0; i < currentPiece.length; i++){
-                currentPiece[i] = currentPiece[i]+10;
+        for(let i = 0; i < currentPieceIdx["I"]["POS1_IDX"].length; i++){
+            makeBlank(currentPieceIdx["I"]["POS1_IDX"][i]);
+        }
+        if(currentPieceIdx["I"]["POS2_IDX"][3]+10 < boardArr.length){
+            currentPieceIdx["I"]["POS2_IDX"].forEach((idx)=>{
+                makeBlank(idx);
+            });
+            for(let i = 0; i < currentPieceIdx["I"]["POS2_IDX"].length; i++){
+                currentPieceIdx["I"]["POS2_IDX"][i] += 10;
             }
             changeSquare();
         } else {
-            const scoreBoard = document.querySelector('.score');
-            scoreBoard.innerText = `Total Score: 100`;
-            console.log('end reached');
-            // currentPiece = currentPieceIdx["L"];
+            console.log('blah');
             clearInterval(pieceFallingInterval);
-        } 
+        }
+        // if(currentPiece[3]+10 < boardArr.length){
+        //     makeBlank(currentPiece[0]);
+        //     for(let i = 0; i < currentPiece.length; i++){
+        //         currentPiece[i] = currentPiece[i]+10;
+        //     }
+        //     changeSquare();
+        // } else {
+        //     const scoreBoard = document.querySelector('.score');
+        //     scoreBoard.innerText = `Total Score: 100`;
+        //     console.log('end reached');
+        //     // currentPiece = currentPieceIdx["L"];
+        //     clearInterval(pieceFallingInterval);
+        // } 
     }
 }
 
